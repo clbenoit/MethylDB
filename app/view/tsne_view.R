fd8095a924909a14fd3f2977920b2105207374f5
clbenoit <benoitclement.sand@gmail.com>
2024-10-31 14:18:39 +0100

    Annotate points on T SNE

--- app/view/tsne_view.R
+++ app/view/tsne_view.R
@@ -9,7 +9,8 @@ box::use(
   dplyr[filter, `%>%`, select, case_when, mutate, arrange, inner_join, rename],
   reactable,
   stats[na.omit],
-  plotly[plotlyOutput, renderPlotly, layout, plot_ly],
+  plotly[plotlyOutput, renderPlotly, layout, plot_ly,
+         plotlyProxy, plotlyProxyInvoke],
   htmlwidgets[onRender],
 )
 
@@ -19,18 +20,24 @@ box::use(
 )
 
 #' @export
+#' @export
 ui <- function(id) {
   ns <- NS(id)
   tagList(
     fluidRow(
       column(width = 12,
-            div(style = "width: 100%; text-align: center;", # Center-align the radio buttons
-            radioButtons(ns("color_by") , label = "color TSNE by" ,
-                            inline = TRUE,
-                            choices = c("class", "subclass", "cohort"),
-                            width = "100%")),
-      ), br(),
+             div(style = "width: 100%; text-align: center;",
+                 radioButtons(ns("color_by"), label = "Color TSNE by",
+                              inline = TRUE,
+                              choices = c("class", "subclass", "cohort"),
+                              width = "100%")
+             )
+      ),
+      br(),
       column(width = 12,
+             selectInput(ns("sample_selector"), "Select Samples to annotate on graph",
+                         choices = NULL, width = '100%',
+                         multiple = TRUE),  # Enable multiple selection
              plotlyOutput(ns("current_tsne_plot"), height = "800px"))
     )
   )
@@ -41,82 +48,93 @@ server <- function(id, con, appData, main_session) {
   moduleServer(id, function(input, output, session) {
 
     ns <- session$ns
+    annotations <- shiny::reactiveVal(list())  # Store plot annotations
 
+    # Update selectInput choices dynamically
+    observeEvent(appData$data$current_samples_dataframe$sample, {
+      shiny::updateSelectInput(session, inputId = "sample_selector",
+                               choices = appData$data$current_samples_dataframe$sample)
+    })
+
+    # Reactive for current t-SNE data frame
     current_tsne_dataframe <- reactive({
-      req(appData$data$BValsC)
-      req(appData$data$current_samples_dataframe)
-      print("selecting variables...")
-      BValsC <- appData$BValsC
+      req(appData$data$BValsC, appData$data$current_samples_dataframe)
+      BValsC <- appData$data$BValsC
       rownames(BValsC) <- BValsC$cgID
       BValsC$cgId <- NULL
-      BValsC <- appData$data$BValsC[,appData$data$current_samples_dataframe$sample]
+      BValsC <- BValsC[, appData$data$current_samples_dataframe$sample]
+      selectVariables(data = na.omit(BValsC), method = "SD", no.variables = 25000, threads = 13)
+    }) %>% bindCache(list(appData$data$BValsC, appData$data$current_samples_dataframe))
 
-      puce_samples <- colnames(BValsC)
-
-      meth <- selectVariables(data = na.omit(BValsC), method = "SD", no.variables = 25000, threads = 13)
-
-      save(list = c("meth", "BValsC"), file = "~/meth.rda")
-
-      return(meth)
-    }) %>% bindCache({list(appData$data$BValsC, appData$data$current_samples_dataframe)})
-
-
+    # Reactive for current t-SNE model
     current_tsne_model <- reactive({
       req(current_tsne_dataframe())
-      print("buidling t sne model...")
-      print(utils::head(current_tsne_dataframe()))
-      tsne2 <- modelTsne(current_tsne_dataframe(),
-                         #group = "cohort",
-                         perplexity = 1, dims = 2)
-      return(tsne2)
+      modelTsne(current_tsne_dataframe(), perplexity = 1, dims = 2)
     }) %>% bindCache(current_tsne_dataframe())
 
+    # Render t-SNE plot
     output$current_tsne_plot <- renderPlotly({
-      req(current_tsne_model())
-      req(appData$data$current_samples_dataframe)
-      req(input$color_by)
-      print("rendering t sne plot...")
-
+      req(current_tsne_model(), appData$data$current_samples_dataframe, input$color_by)
       df <- current_tsne_model()$df.tsne
-      return(plot_ly(x = df$tsne.dim1,
-                  y = df$tsne.dim2,
-                  type = "scatter",
-                  mode = "markers",
-                  color = appData$data$current_samples_dataframe[,input$color_by],
-                  text = ~paste("Sample: ", df$sample),
-                  hoverinfo = 'text',  # Only display the custom text (no x/y info)
-                  marker = list(size = 10),
-                  # marker = list(size = ~ifelse(df$is_special, 8, 6),  # Larger points for special samples
-                  #               symbol = ~ifelse(df$is_special, 'x', 'circle'))
-                  ) %>%
+
+      plot_ly(
+        x = df$tsne.dim1, y = df$tsne.dim2,
+        type = "scatter", mode = "markers",
+        color = appData$data$current_samples_dataframe[, input$color_by],
+        text = ~paste("Sample: ", df$sample),
+        hoverinfo = 'text',
+        marker = list(size = 10)
+      ) %>%
         layout(
           title = "2D t-SNE",
           xaxis = list(title = "t-SNE Dimension 1", zeroline = FALSE),
           yaxis = list(title = "t-SNE Dimension 2", zeroline = FALSE)
-        )
-    %>%
-        onRender("
-      function(el, x) {
-        el.on('plotly_click', function(d) {
-          var sampleName = d.points[0].text.split(': ')[1];  // Extract sample name from hover text
-        // Update the layout to show an annotation with the sample name
-        Plotly.relayout(el, {
-          annotations: [{
-            x: d.points[0].x,
-            y: d.points[0].y,
-            text: 'Sample: ' + sampleName,
-            showarrow: true,
-            arrowhead: 7
-            }]
-          });
-        });
-      }
-    ")
-        )
-        }) %>% bindCache(list(current_tsne_model(),
-                              appData$data$current_samples_dataframe,
-                              input$color_by))
+        ) %>%
+        onRender(sprintf("
+        function(el, x) {
+          el.on('plotly_click', function(d) {
+            var sampleName = d.points[0].text;
 
+            // Add sample name to Shiny input with namespace
+            Shiny.setInputValue('%s', sampleName, {priority: 'event'});
+          });
+        }
+      ", ns("sample_click")))
+    })
+
+    # Observe plotly click to update selectInput for multiple selections
+    observeEvent(input$sample_click, {
+      sample_name <- strsplit(input$sample_click, "[: ]+")[[1]][2]
+      selected_samples <- input$sample_selector
+
+      # Update selectInput to include clicked sample if not already selected
+      if (!sample_name %in% selected_samples) {
+        selected_samples <- c(selected_samples, sample_name)
+        shiny::updateSelectInput(session, "sample_selector", selected = selected_samples)
+      }
+    })
+
+    # Observe selectInput changes to manage annotations on the plot
+    observeEvent(input$sample_selector, {
+      req(input$sample_selector, current_tsne_model())
+      df <- current_tsne_model()$df.tsne
+
+      # Gather annotations for each selected sample
+      new_annotations <- lapply(input$sample_selector, function(sample) {
+        point <- df %>% filter(sample == !!sample)
+        list(
+          x = point$tsne.dim1,
+          y = point$tsne.dim2,
+          text = paste("Sample:", sample),
+          showarrow = TRUE,
+          arrowhead = 7
+        )
+      })
+
+      # Update stored annotations and apply to the plot
+      annotations(new_annotations)
+      plotlyProxy("current_tsne_plot", session) %>%
+        plotlyProxyInvoke("relayout", list(annotations = new_annotations))
+    })
   })
 }
-
